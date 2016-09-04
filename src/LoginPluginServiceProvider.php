649637c7a481dbb4b3bb9f09e658bb7f6103a76d
marcmascarell <marcmascarell@gmail.com>
2016-09-04 21:03:37 +0200

    = remove comments

--- src/LoginPluginServiceProvider.php
+++ src/LoginPluginServiceProvider.php
@@ -5,19 +5,9 @@ class LoginPluginServiceProvider extends ArtificerExtensionServiceProvider {
 
     protected $package = 'mascame/login';
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->addPlugin(LoginPlugin::class);
     }
-
-//    public function register()
-//    {
-//
-//    }
 
 }
